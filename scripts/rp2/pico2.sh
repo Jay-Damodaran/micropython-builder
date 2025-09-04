0701728538d3fdcad013cb9459e24eeb6b27ba02
Jay-Damodaran <146678205+Jay-Damodaran@users.noreply.github.com>
2025-09-04 16:29:37 -0500

    Altered pico2.sh
    Modified structure to be more consistent with the other .sh scripts

--- scripts/rp2/pico2.sh
+++ scripts/rp2/pico2.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
-sudo apt update
-sudo apt install net-tools
-sudo apt install git
-sudo apt install chromium
-sudo apt install build-essential
-sudo apt install cmake
-sudo apt install ninja-build
-sudo apt install libusb-1.0-0-dev
-sudo apt install gcc-arm-none-eabi
 
-git clone https://github.com/micropython/micropython.git
-git clone https://github.com/v923z/micropython-ulab.git ulab
+source ./scripts/rp2_init.sh
 
-cd micropython
-git submodule update --init lib/tinyusb
-git submodule update --init lib/pico-sdk
-cd lib/pico-sdk
-git submodule update --init lib/tinyusb
-
-cd ../..
-make -C mpy-cross
-cd ports/rp2
+cd micropython/ports/rp2
 make BOARD=RPI_PICO2 submodules
 make BOARD=RPI_PICO2 clean
 make BOARD=RPI_PICO2 USER_C_MODULES=../../../ulab/code/micropython.cmake
+cd ../../..
+
+copy_files rp2/build-RPI_PICO2/firmware.uf2 RPI_PICO2
+clean_up rp2 build-RPI_PICO2
