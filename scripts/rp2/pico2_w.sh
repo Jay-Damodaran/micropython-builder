a4ef435fc7f69b436818b8f87fecd849a0043287
Jay-Damodaran <146678205+Jay-Damodaran@users.noreply.github.com>
2025-09-04 16:31:45 -0500

    Update pico2_w.sh
    Made file contents more aligned with sh file structure of other files in this directory.

--- scripts/rp2/pico2_w.sh
+++ scripts/rp2/pico2_w.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
-sudo apt update
-sudo apt install net-tools
-sudo apt install git
-sudo apt install chromium
-sudo apt install build-essential
-sudo apt install cmake
-sudo apt install ninja-build
-sudo apt install libusb-1.0-0-dev
-sudo apt install gcc-arm-none-eabi
 
-mkdir pico
-cd pico
-git clone https://github.com/micropython/micropython.git
-git clone https://github.com/v923z/micropython-ulab.git ulab
+source ./scripts/rp2_init.sh
 
-cd micropython
-git submodule update --init lib/tinyusb
-git submodule update --init lib/pico-sdk
-cd lib/pico-sdk
-git submodule update --init lib/tinyusb
-
-cd ../..
-make -C mpy-cross
-cd ports/rp2
+cd micropython/ports/rp2
 make BOARD=RPI_PICO2_W submodules
 make BOARD=RPI_PICO2_W clean
 make BOARD=RPI_PICO2_W USER_C_MODULES=../../../ulab/code/micropython.cmake
+cd ../../..
+
+copy_files rp2/build-RPI_PICO2_W/firmware.uf2 RPI_PICO2_W
+clean_up rp2 build-RPI_PICO2_W
